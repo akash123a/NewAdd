01289de14b35f936f825231f1808eb1e70843ef5
akash20g <142041851+akash20g@users.noreply.github.com>
2024-12-07 23:45:34 +0530

    Accio file added

--- src/Lion.java
+++ src/Lion.java
@@ -1,4 +1,5 @@
 public class Lion {
     int hello;
     int Hello;
+    System.out.print("Hello Akash");
 }
---
+++ src/Master.java
@@ -0,0 +1,4 @@
+public class Master {
+    System.out.print("You can Call me master");
+    System.out.print("Hello AJash ");
+}
